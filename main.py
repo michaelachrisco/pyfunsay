66925bce5e372afe72cb316b141a079401fee3ae
Michael Chrisco <michaelachrisco@gmail.com>
2021-02-23 19:22:33 -0800

    having more fun with open weather api

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import random
 import pyttsx3
+import requests
 
-from essential_generators import DocumentGenerator
-gen = DocumentGenerator()
+# get grid from: https://api.weather.gov/points/36.7627,-119.7988
+weather_request = requests.get('https://api.weather.gov/gridpoints/HNX/51,101/forecast')
 
+# kludgy but works!!
+weather_request_periods = weather_request.json()['properties']['periods']
 engine = pyttsx3.init()
 engine.runAndWait()
-sentence = gen.sentence() + "."
-print(sentence)
 
-engine.setProperty('rate', 180)
+engine.setProperty('rate', 130)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[16].id)
-engine.say('hello world!')
-engine.say(sentence)
+
+engine.say('The weather is currently: ')
+engine.say(weather_request_periods[0]['detailedForecast'].replace('mph', 'miles per hour'))
+
+engine.say('The weather tomorrow is: ')
+engine.say(weather_request_periods[1]['detailedForecast'].replace('mph', 'miles per hour'))
 
 engine.runAndWait()
